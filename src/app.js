fe53a364dd14941a139aa9c5da9aac2f0b6e4ff5
Kakomanato <160876603+Kakomanato@users.noreply.github.com>
2024-03-13 17:05:07 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,43 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let excuse = "";
+
+  let randomWho = Math.floor(Math.random() * who.length);
+  let randomWhoLeader = who[randomWho];
+
+  let randomAction = Math.floor(Math.random() * action.length);
+  let randomActionLeader = action[randomAction];
+
+  let randomWhat = Math.floor(Math.random() * what.length);
+  let randomWhatLeader = what[randomWhat];
+
+  let randomWhen = Math.floor(Math.random() * when.length);
+  let randomWhenLeader = when[randomWhen];
+
+  excuse +=
+    randomWhoLeader +
+    " " +
+    randomActionLeader +
+    " " +
+    randomWhatLeader +
+    " " +
+    randomWhenLeader +
+    "\n";
+
+  document.getElementById("excuse").innerHTML = excuse;
+  console.log(excuse);
 };
